041ca99dc5ba47cb4e84982f6595f513774afd6a
tuppsies <tupperwarec@hotmail.com>
2021-12-21 21:37:05 +1100

    Add shutdown script

---
+++ shutdown-if-inactive.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Source:
+# https://github.com/fernandoacorreia/dotfiles/blob/4d0d8cc4df33504352c032978759a6bd730eb957/bin/shutdown-if-inactive
+#
+# Shuts down the host on inactivity.
+#
+# Designed to be executed as root from a cron job.
+# It will power off on the 2nd consecutive run without an active ssh session.
+# That prevents an undesirable shutdown when the machine was just started, or on a brief disconnect.
+#
+# To enable, add this entry to /etc/crontab:
+# */5 *   * * *   root    /home/ubuntu/dotfiles/bin/shutdown-if-inactive
+#
+set -o nounset -o errexit -o pipefail
+
+MARKER_FILE="/tmp/ssh-inactivity-flag"
+
+STATUS=$(netstat | grep ssh | grep ESTABLISHED &>/dev/null && echo active || echo inactive)
+
+if [ "$STATUS" == "inactive" ]; then
+  if [ -f "$MARKER_FILE" ]; then
+    echo "Powering off due to ssh inactivity."
+    poweroff  # See https://unix.stackexchange.com/a/196014/56711
+  else
+    # Create a marker file so that it will shut down if still inactive on the next time this script runs.
+    touch "$MARKER_FILE"
+  fi
+else
+  # Delete marker file if it exists
+  rm --force "$MARKER_FILE"
+fi
